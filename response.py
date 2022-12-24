8b98703f2e021eb85a94b23a75011f49601576e2
aidan ryther <aidanryther15@gmail.com>
2022-12-23 20:33:04 -0500

    .

--- response.py
+++ response.py
@@ -1,19 +1,19 @@
 import random
 
 
-def get_response(message: str) -> str:
+def get_response(message: str):
     p_message = message.lower()
 
-    if p_message == 'hello':
-        return 'hey there!'
+    if p_message == '!move':
+        
 
-    if message == 'roll':
+    if message == '!random':
         return str(random.randint(1, 6))
 
     if p_message == '!help':
-        return '`this is a help message that you can modify.`'
+        return '`!random-random number between 1-6\n`!move-moves you to different channel'
 
-    return 'I didn\'t understand what you wrote. Try typing "!help".'
+   
 
 
     
