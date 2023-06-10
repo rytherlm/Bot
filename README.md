2b3d9b66eb8c49e44819569a8112590b08ce5d63
arr9180 <111656411+arr9180@users.noreply.github.com>
2023-06-10 13:59:50 -0400

    .

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# Bot
-This is where I am making my discord bot!
+# Discord Bot
+![Example Image of the Bot In a Server](Bot/photo/Screenshot 2023-06-10 at 1.58.39 PM.png)
+
+
+
+
+This is a versatile and feature-rich Discord bot written in Python using the discord.py library. The bot provides a wide range of commands to enhance your Discord server experience and engage with your community. It offers useful utilities, fun games, moderation tools, and more.
+
+# Features
+
+Random Command: Generate a random number between 1 and 10 with the !random command.
+Time Command: Get the current time in your timezone using the !time command.
+Flip Command: Flip a coin and get either 'heads' or 'tails' with the !flip command.
+Clear Command: Delete messages in a channel with the !clear command (requires the 'manage_messages' permission).
+Help Command: Access the list of available commands and their descriptions using the !help command.
+Welcome Message: Automatically sends a welcome message when a new member joins the server.
+Usage
+
+Invite the bot to your Discord server using the invite link provided by the bot developer.
+Ensure the bot has the necessary permissions to function correctly (e.g., read messages, send messages, manage messages).
+Use the command prefix ! followed by the command name to execute a specific command. For example, use !random to generate a random number.
+Refer to the help command (!help) for a complete list of available commands and their descriptions.
+Installation
+
+To run the Discord bot locally or on your own server, follow these steps:
+
+Clone the repository or download the source code.
+Install the required dependencies by running the command pip install -r requirements.txt.
+Obtain a bot token from the Discord Developer Portal and replace 'YOUR_TOKEN_HERE' in the code with your token.
+Optionally, add your own bot image by replacing your_image_url_here with the URL of your desired image.
+Run the bot by executing the command python bot.py in your terminal or command prompt.
+Contributing
+
+Contributions to the Discord bot are welcome! If you have any ideas, bug fixes, or feature enhancements, feel free to submit a pull request. Please ensure your code follows the existing coding style and includes appropriate tests if necessary.
+
+# License
+
+This Discord bot is licensed under the MIT License.
+
+Acknowledgements
+
+The development of this Discord bot was made possible by the discord.py library and the vibrant Discord developer community.
+
+# # Contact
+
+For any inquiries or support regarding the Discord bot, please contact Aidan Ryther at aidanryther15@gmail.com.
--- bot.py
+++ bot.py
@@ -1,54 +1,62 @@
 import discord
+from discord.ext import commands
 import datetime
 import random
 
-def get_response(message, user_permissions):
-    message = message.lower()
-    if message == 'random':
-        return str(random.randint(1, 10))
-    elif message == 'time':
-        current_time = datetime.datetime.now().strftime("%I:%M %p")
-        return current_time
-    elif message == 'flip':
-        number = random.randint(1,2)
-        if number ==  1:
-            return "heads"
-        else:
-            return 'tails'
-    elif message == 'clear' and user_permissions.manage_messages:
-        return ''
-    elif message == '!help':
-        return '`random\ntime\nflip\nclear`'
-    return "Bad input"
+bot = commands.Bot(command_prefix='!')
 
-# TOKEN =#taken out becuase its mine lol
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is now running!')
 
-@client.event
+@bot.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name="welcome")
     await channel.send(f'Welcome to the server {member.mention}')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} is now running!')
+@bot.command()
+async def random(ctx):
+    """Generates a random number between 1 and 10."""
+    await ctx.send(str(random.randint(1, 10)))
 
-@client.event
-async def on_message(message):
-    if message.author.guild_permissions.manage_messages and message.content.lower() == "clear":
-        await message.channel.purge(limit=None)
-    if message.author == client.user:
-        return
-    user_permissions = message.author.guild_permissions
-    user_message = message.content
-    back = get_response(user_message, user_permissions)
-    if back:
-        await message.channel.send(back)
+@bot.command()
+async def time(ctx):
+    """Displays the current time."""
+    current_time = datetime.datetime.now().strftime("%I:%M %p")
+    await ctx.send(current_time)
+
+@bot.command()
+async def flip(ctx):
+    """Flips a coin and returns 'heads' or 'tails'."""
+    number = random.randint(1, 2)
+    if number == 1:
+        await ctx.send("heads")
+    else:
+        await ctx.send("tails")
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx):
+    """Clears messages in the channel (requires 'manage_messages' permission)."""
+    await ctx.channel.purge(limit=None)
+
+@bot.command()
+async def help(ctx):
+    """Displays a list of available commands."""
+    command_list = [command.name for command in bot.commands]
+    help_text = '\n'.join(command_list)
+    embed = discord.Embed(title="Available Commands", description=help_text, color=discord.Color.blue())
+    await ctx.send(embed=embed)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Invalid command. Use '!help' to see available commands.")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You don't have the required permissions to execute this command.")
 
 def run_discord_bot():
-    client.run(TOKEN)
+    bot.run('YOUR_TOKEN_HERE')
 
 def main():
     run_discord_bot()
